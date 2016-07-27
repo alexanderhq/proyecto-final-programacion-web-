e7af8c4bd35c74a5c684c475731fcbb2cf4be7bb
alexanderhq <alexanderhq13@gmail.com>
2016-07-26 21:42:25 -0400

    primera parte..

---
+++ custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+$("#bcontacto").submit(function(event){
+event.preventDefault();
+
+$.ajax({
+type:'POST',
+url: 'send.php',
+data:$(this).serialize(),
+success: function(data){
+$("#respuesta").slideDown();
+$("#respuesta").html(data);
+
+
+}
+
+})
+
+
+return false;
+
+});
+
+});
